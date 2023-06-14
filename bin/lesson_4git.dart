73c443057097b6f44221722e0b2700b5de05ed2d
AlekseyGel <street-rose@bk.ru>
2023-06-14 17:33:53 +0300

    first commit

---
+++ bin/lesson_4git.dart
@@ -0,0 +1,5 @@
+import 'package:lesson_4git/lesson_4git.dart' as lesson_4git;
+
+void main(List<String> arguments) {
+  print('Hello world: ${lesson_4git.calculate()}!');
+}
---
+++ test/lesson_4git_test.dart
@@ -0,0 +1,8 @@
+import 'package:lesson_4git/lesson_4git.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
